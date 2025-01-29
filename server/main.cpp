fe61ca926cb81535484f0501255c71b05e70baeb
José Ronaldo <dev.joseronaldo@gmail.com>
2025-01-29 11:34:32 -0300

    refactor: switch from Crow framework to cpprest framework

--- server/main.cpp
+++ server/main.cpp
@@ -1,13 +1,45 @@
-#include "./Crow/include/crow.h"
+#include <cpprest/http_listener.h>
+#include <cpprest/json.h>
+#include <iostream>
+#include <thread>
+#include <chrono>
+
+using namespace web;
+using namespace web::http;
+using namespace web::http::experimental::listener;
+
+void say_hello(const http_request& request) {
+    json::value response_data;
+    response_data[U("message")] = json::value::string(U("Hello world!"));
+
+    request.reply(status_codes::OK, response_data);
+}
 
 int main() {
-    crow::SimpleApp app;
+    http_listener listener(U("http://localhost:8080"));
 
-
-    CROW_ROUTE(app, "/hello")
-    ([](){
-        return "Hello, World!";
+    listener.support(methods::GET, [](const http_request& request) {
+        if (request.relative_uri().path() == U("/hello")) {
+            say_hello(request);
+        }
     });
 
-    app.port(8080).multithreaded().run();
+    try {
+
+        listener
+            .open()
+            .then([]() { 
+                std::wcout << L"Servidor rodando em http://localhost:8080" << std::endl; 
+            })
+            .wait();
+
+        // Adicionando um loop de espera para manter o servidor de pé
+        while (true) {
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+        }
+    } catch (const std::exception& e) {
+        std::cerr << "An error occurred: " << e.what() << std::endl;
+    }
+
+    return 0;
 }
