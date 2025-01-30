6a68e2bb107f90174513b27e6cc8453a2a515368
José Ronaldo <dev.joseronaldo@gmail.com>
2025-01-29 21:59:01 -0300

    feat: add Card component

---
+++ frontend/app/components/Card/index.tsx
@@ -0,0 +1,22 @@
+'use client'
+
+import { useRouter } from 'next/navigation';
+
+type CardProps = {
+  label: string;
+  route: string;
+  icon: JSX.Element;
+};
+
+export function Card({ label, route, icon }: CardProps) {
+  const router = useRouter();
+  return (
+    <div
+      className="flex flex-col items-center bg-primary text-white p-6 rounded-lg shadow-lg w-60 cursor-pointer hover:bg-red-800"
+      onClick={() => router.push(route)}
+    >
+      <div className="text-5xl mb-4">{icon}</div>
+      <p className="text-xl font-semibold">{label}</p>
+    </div>
+  );
+}
