bf1d3d1daf5f597b33ecdaa09e2dbd3c20d96551
José Ronaldo <dev.joseronaldo@gmail.com>
2025-01-29 21:59:27 -0300

    feat: add Home page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,15 @@
+import { Card } from './components/Button';
+import { FaUserGraduate, FaChalkboardTeacher, FaBuilding } from 'react-icons/fa';
+
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h1>LKCIn</h1>
-      </main>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-white text-black p-6">
+      <h1 className="text-7xl font-bold mb-14">LkCIn</h1>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-6 w-full max-w-3xl">
+        <Card label="Sou Aluno" route="/login/aluno" icon={<FaUserGraduate />} />
+        <Card label="Sou Professor" route="/login/professor" icon={<FaChalkboardTeacher />} />
+        <Card label="Sou Empresa" route="/login/empresa" icon={<FaBuilding />} />
+      </div>
     </div>
   );
 }
